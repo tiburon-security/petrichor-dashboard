1640015b35ecc0fc4e53715726dd45f7835f76d4
Jovanni Hernandez <jovanni.m.hernandez@gmail.com>
2017-01-20 20:38:55 -0500

    closes #1, it wasn 't actually a bug, just misused setInterval instead of setTimeout for example widget

--- src/Dashboard/Widgets/FakeWidget4.js
+++ src/Dashboard/Widgets/FakeWidget4.js
@@ -14,14 +14,14 @@ class FakeWidget4 extends Component {
 		console.debug("FakeWidget4 mounted");
 		
 		// Simulate loading data for 5 seconds
-		setInterval(() => { this.setState({loading:false}) }, 5000);
+		setTimeout(() => { this.setState({loading:false}) }, 5000);
 	}
 	
 	render() {
     
 		return (  
 			<FullWidget settings_button={true} close_button={true} title="FakeWidget4" {...this.props} loading={this.state.loading}>
-				Fake data
+				<dookie>Fake data</dookie>
 				<a href="#" onClick={() =>{this.setState({loading:!this.state.loading})}}>click</a>
 			</FullWidget>
 		);
