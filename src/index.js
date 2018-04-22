9740cef81d404b234b81ee3cb1da12d1167334e5
Jovanni Hernandez <jovanni.m.hernandez@gmail.com>
2018-04-22 20:20:55 +0100

    Fixed issue with redux blocking changes when used w/ react-router

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import Gentella from './Theme/Gentella.js';
 import { Provider } from 'react-redux';
 import store, { history } from './redux/store/configureStore';
 import { ConnectedRouter } from 'react-router-redux'
+import { Route } from 'react-router';
 
 fetch('/routes_menu_config.json')
 	.then(function(response) {
@@ -17,20 +18,22 @@ function bootstrapApplication(config){
 	// Store the config as a global,  since it will be reference in othermaybe
 	// parts of the app, maybe there's a better way of doing this...?
 	window.app_config = config;
-	
-	//var dynamicRoutesJSX = buildDynamicRoutes(config, store);
-	
+		
 	// Hook the content body of gentella, this is where page content
 	// will be rendered. Routes control this body content
 	ReactDOM.render((
 	<Provider store={store}>
 		<ConnectedRouter history={history}>
-			<Gentella config={config} />
 		
-		{/*<Switch>
-				{dynamicRoutesJSX}
-		</Switch>*/}
-		
+			{/*
+			Pathless route to ensure child components are re-rendered on location changes when using React-Redux
+			https://github.com/ReactTraining/react-router/blob/master/packages/react-router/docs/guides/blocked-updates.md
+			*/}
+			<Route 
+				render={(props)=>(
+					<Gentella config={config} location={props.location} />
+				)}
+			/>
 		</ConnectedRouter>
 		</Provider>
 	     
