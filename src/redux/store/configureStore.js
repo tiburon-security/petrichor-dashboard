06ca5b7e86c18971d4ae1a5d1cf2bd50bda7d6f5
Jovanni Hernandez <jovanni.m.hernandez@gmail.com>
2018-07-10 06:29:53 +0100

    fixed redux extension middleware declaration so page still renders if browser doesnt have redux extension

--- src/redux/store/configureStore.js
+++ src/redux/store/configureStore.js
@@ -1,4 +1,4 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from '../reducers';
 import { routerMiddleware } from 'react-router-redux'
@@ -14,14 +14,14 @@ const middleware = [
 	routerMiddleware(history)
 ]
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__;
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(
 	rootReducer,
 	initialState,
 	composeEnhancers(
 		applyMiddleware(...middleware)
-	)
+	),
 );
 
 export default store;
