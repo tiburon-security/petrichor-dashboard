debc8bcfb1d4140dd4ba179f93c3a642a8274931
Jovanni Hernandez <jovanni.m.hernandez@gmail.com>
2018-07-21 20:26:24 +0100

    Missed a few react-router-redux dependencies

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -2,11 +2,9 @@ import { combineReducers } from 'redux';
 import { popupModal } from './PopupModal';
 import { sidebar } from './SidebarMenu';
 import { dashboard } from './Dashboard';
-import { routerReducer } from 'react-router-redux'
 
 export default combineReducers({
 	popupModal,
 	sidebar,
 	dashboard,
-	routing: routerReducer
 });
