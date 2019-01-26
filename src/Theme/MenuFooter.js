0902c27466e203f9f68d18e14db54dc71c744b77
Jovanni Hernandez <jovanni.m.hernandez@gmail.com>
2019-01-26 13:33:35 +0000

    Missed during last commit; implements menu footer hiding feature

--- src/Theme/MenuFooter.js
+++ src/Theme/MenuFooter.js
@@ -3,27 +3,33 @@ import React, { Component } from 'react';
 class MenuFooter extends Component {	
 	
 	render() {
+
 		return (
-            
-            
+
             <div className="sidebar-footer hidden-small">
-              <a data-toggle="tooltip" data-placement="top" title="Settings">
-                <span className="glyphicon glyphicon-cog" aria-hidden="true"></span>
-              </a>
-              <a data-toggle="tooltip" data-placement="top" title="FullScreen">
-                <span className="glyphicon glyphicon-fullscreen" aria-hidden="true"></span>
-              </a>
-              <a data-toggle="tooltip" data-placement="top" title="Lock">
-                <span className="glyphicon glyphicon-eye-close" aria-hidden="true"></span>
-              </a>
-              <a data-toggle="tooltip" data-placement="top" title="Logout">
-                <span className="glyphicon glyphicon-off" aria-hidden="true"></span>
-              </a>
-            </div>
-           
-            
+				{
+					this.props.config.show_menu_footer && (
+						<div>
+							<a data-toggle="tooltip" data-placement="top" title="Settings">
+								<span className="glyphicon glyphicon-cog" aria-hidden="true"></span>
+							</a>
+							<a data-toggle="tooltip" data-placement="top" title="FullScreen">
+								<span className="glyphicon glyphicon-fullscreen" aria-hidden="true"></span>
+							</a>
+							<a data-toggle="tooltip" data-placement="top" title="Lock">
+								<span className="glyphicon glyphicon-eye-close" aria-hidden="true"></span>
+							</a>
+							<a data-toggle="tooltip" data-placement="top" title="Logout">
+								<span className="glyphicon glyphicon-off" aria-hidden="true"></span>
+							</a>
+						</div>
+					)
+				}
+            </div>            
 		);
 	}
 }
+/*
 
+*/
 export default MenuFooter
